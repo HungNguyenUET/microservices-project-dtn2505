efdd48e84ec39f14b8a9776c56c750414b1621b4
hungnguyenuet <hungnguyenuet@gmail.com>
2025-11-11 21:06:07 +0700

    Add ValidationException

--- auth-service/src/main/java/vti/dtn/auth_service/config/SecurityConfiguration.java
+++ auth-service/src/main/java/vti/dtn/auth_service/config/SecurityConfiguration.java
@@ -8,17 +8,39 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import vti.dtn.auth_service.filter.JwtFilter;
+import vti.dtn.auth_service.oauth2.handler.OAuth2AuthenticationFailureHandler;
+import vti.dtn.auth_service.oauth2.handler.OAuth2AuthenticationSuccessHandler;
+import vti.dtn.auth_service.oauth2.repository.HttpCookieOAuthorizationRequestRepository;
+import vti.dtn.auth_service.oauth2.service.CustomOAuth2UserService;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
     private static final String[] WHITE_LIST_URL = {
+            "/oauth2/redirect",
+            "/oauth2/authorize",
+            "/oauth2/authorize/github",
+            "/oauth2/callback/github",
+            "/oauth2/redirect",
             "/api/v1/auth/user/register",
             "/api/v1/auth/login",
             "/api/v1/auth/refresh-token",
             "/api/v1/auth/verify"
     };
+
+    private final JwtFilter jwtFilter;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+    private final HttpCookieOAuthorizationRequestRepository httpCookieOAuthorizationRequestRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,8 +51,25 @@ public class SecurityConfiguration {
                                 .permitAll()
                                 .anyRequest().authenticated()
                 )
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .oauth2Login(oauth2 -> oauth2
+                        .authorizationEndpoint(authorization -> authorization
+                                .baseUri("/oauth2/authorize")
+                                .authorizationRequestRepository(httpCookieOAuthorizationRequestRepository)
+                        )
+                        .redirectionEndpoint(redirection -> redirection
+                                .baseUri("/oauth2/callback/*")
+                        )
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService))
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                        .failureHandler(oAuth2AuthenticationFailureHandler)
                 );
 
         return http.build();
--- auth-service/src/main/java/vti/dtn/auth_service/oauth2/user/UserPrincipal.java
+++ auth-service/src/main/java/vti/dtn/auth_service/oauth2/user/UserPrincipal.java
@@ -3,11 +3,17 @@ package vti.dtn.auth_service.oauth2.user;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import vti.dtn.auth_service.entity.UserEntity;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+
+import static vti.dtn.auth_service.oauth2.common.OAuth2Constant.ROLE_USER;
 
 @Setter
 @AllArgsConstructor
@@ -16,6 +22,11 @@ public class UserPrincipal implements OAuth2User, UserDetails {
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
     private Map<String, Object> attributes;
+
+    public static UserPrincipal create(UserEntity user) {
+        List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(ROLE_USER));
+        return new UserPrincipal(user.getUsername(), user.getPassword(), authorities, Map.of());
+    }
 
     @Override
     public Map<String, Object> getAttributes() {
